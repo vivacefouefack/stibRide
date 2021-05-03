46b2a6f759937ab70908b43728d61811e64afadd
vivac <54490@etu.he2b.be>
2021-05-03 15:02:58 +0200

    mise en place de l'architecture model view presenter(MVP)

---
+++ src/main/java/g54490/atlg4/stib/model/Graphe.java
@@ -0,0 +1,9 @@
+package g54490.atlg4.stib.model;
+
+/**
+ *
+ * @author 54490@etu.he2b.be
+ */
+public class Graphe {
+    
+}
---
+++ src/main/java/g54490/atlg4/stib/presenter/Presenter.java
@@ -0,0 +1,9 @@
+package g54490.atlg4.stib.presenter;
+
+/**
+ *
+ * @author 54490@etu.he2b.be
+ */
+public class Presenter {
+    
+}
