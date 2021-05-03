61438ab2aacaa8c434c41941a068f0a495e23185
vivac <54490@etu.he2b.be>
2021-05-03 15:17:19 +0200

    mise en place des packages nécessaires pour l'implementation du design  pattern repository

---
+++ src/main/java/g54490/atlg4/stibride/dto/Line.java
@@ -0,0 +1,9 @@
+package g54490.atlg4.stibride.dto;
+
+/**
+ *
+ * @author 54490@etu.he2b.be
+ */
+public class Line {
+    
+}
---
+++ src/main/java/g54490/atlg4/stibride/repository/Dao.java
@@ -0,0 +1,11 @@
+package g54490.atlg4.stibride.repository;
+
+
+/**
+ *
+ * @author 54490@etu.he2b.be
+ */
+public interface Dao {
+
+   
+}
