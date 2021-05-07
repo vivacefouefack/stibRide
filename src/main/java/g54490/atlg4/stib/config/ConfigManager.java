fca1055d9cfa5b941611c78ae6cfd4560da06d17
vivac <54490@etu.he2b.be>
2021-05-07 13:12:21 +0200

    ajout du fichier properties et création de la classe configManager

---
+++ src/main/java/g54490/atlg4/stib/config/ConfigManager.java
@@ -0,0 +1,66 @@
+package g54490.atlg4.stib.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * This singleton gives access to all properties from config.properties.
+ *
+ * @author 54490@etu.he2b.be
+ */
+public class ConfigManager {
+
+    private static final String FILE = "./config/config.properties";
+
+    private final Properties prop;
+
+    private final String url;
+
+    /**
+     * private constructor
+     */
+    private ConfigManager() {
+        prop = new Properties();
+        url = getClass().getClassLoader().getResource(FILE).getFile();
+    }
+
+    /**
+     * Loads the properties from this url.
+     *
+     * @throws IOException if no file is found.
+     */
+    public void load() throws IOException {
+
+        try (InputStream input = new FileInputStream(url)) {
+            prop.load(input);
+        } catch (IOException ex) {
+            throw new IOException("Chargement configuration impossible ", ex);
+        }
+    }
+
+    /**
+     * Returns the value from the key name.
+     *
+     * @param name key to found.
+     * @return the value from the key-value pair.
+     */
+    public String getProperties(String name) {
+        return prop.getProperty(name);
+    }
+
+    /**
+     * Returns the instance of the singleton.
+     *
+     * @return the instance of the singleton.
+     */
+    public static ConfigManager getInstance() {
+        return ConfigManagerHolder.INSTANCE;
+    }
+
+    private static class ConfigManagerHolder {
+
+        private static final ConfigManager INSTANCE = new ConfigManager();
+    }
+}
