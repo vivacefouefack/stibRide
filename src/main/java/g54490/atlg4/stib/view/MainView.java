0654065bc1637379759a662e76fe5e37cf57fdbf
vivac <54490@etu.he2b.be>
2021-05-07 13:00:18 +0200

    intégration du fichier fxml(stipInterface.fxml) dans le projet.

--- src/main/java/g54490/atlg4/stib/view/MainView.java
+++ src/main/java/g54490/atlg4/stib/view/MainView.java
@@ -1,9 +1,28 @@
 package g54490.atlg4.stib.view;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 54490@etu.he2b.be
  */
-public class MainView {
+public class MainView extends Application{
     
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root=FXMLLoader.load(getClass().getResource("/fxml/stibInterface.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.getIcons().add(new Image("/icons/logo.png"));
+        stage.setTitle("Stib Ride");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
--- src/main/java/g54490/atlg4/stib/view/MainViewController.java
+++ src/main/java/g54490/atlg4/stib/view/MainViewController.java
@@ -1,9 +1,54 @@
 package g54490.atlg4.stib.view;
 
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.SearchableComboBox;
+
 /**
  *
- * @author vivac
+ * @author 54490@etu.he2b.be
  */
-public class MainViewController {
+public class MainViewController implements Initializable{
+    
+    @FXML private SearchableComboBox<String> destination;
+    @FXML private SearchableComboBox<String> origine;
+    @FXML private ImageView image1;
+    @FXML private AnchorPane parent;
+    @FXML private Button search;
+    @FXML private Label erreur;
+    
+    @FXML void launchFavoritePage(ActionEvent event)  {
+       
+    }
+
+    @FXML private void search(ActionEvent event) {
+        
+    }
+
+    private AnchorPane parentHome;
+
+
+    /**
+     * constructor of mainViewController.
+     */
+    public MainViewController() {
+
+        this.destination = new SearchableComboBox<String>();
+        this.origine = new SearchableComboBox<String>();
+        this.erreur = new Label();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
     
 }
